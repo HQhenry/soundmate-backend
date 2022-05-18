6a616d12d08225b1458caab25dcc604dd3195ca7
proehr <pauline.roehr@yahoo.de>
2022-05-18 11:54:10 +0200

    3-setup-backend: Add base spring boot project

---
+++ src/test/java/com/edu/basaoc/SpotifriendsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edu.basaoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpotifriendsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
